b3306711fd9c26cba373b2afdfb3e5c959ab7184
Karan Parmar <parmarkaran1976@gmail.com>
2023-12-17 20:43:27 +0530

    changed the profile picture

--- components/MainBanner.tsx
+++ components/MainBanner.tsx
@@ -6,12 +6,12 @@ import Link from "next/link";
 const MainBanner = () => {
   return (
     <AnimatePresence>
-      <div className="flex flex-col-reverse items-center md:flex-row w-full justify-center pt-[8rem] pb-[15rem] bg-whitish px-8 md:px-0 gap-4">
-        <div className="flex flex-col w-[90%] lg:max-w-[55%] items-start">
-          <span className="text-3xl md:text-6xl font-medium capitalize">
+      <div className="flex flex-col-reverse items-center md:flex-row w-full justify-center pt-[4rem] md:pt-[8rem] pb-[15rem] bg-whitish px-8 md:px-8 gap-4">
+        <div className="flex flex-col w-[100%] lg:max-w-[55%] items-center md:items-start">
+          <span className="text-2xl md:text-6xl font-medium capitalize text-center md:text-left">
            Empowering you to achieve you best physical potential.
           </span>
-          <span className="mt-6 uppercase text-red font-semibold">
+          <span className="mt-6 uppercase text-red font-semibold text-center md:text-left">
             Dr Krutee Sangani | BPT | Certified Yoga Therapist.
           </span>
           <Link
@@ -28,18 +28,19 @@ const MainBanner = () => {
         </div>
 
         <motion.div
-          className="relative shrink-0"
+          className="relative shrink-0 h-[340px] w-[340px] rounded-full overflow-hidden"
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
+
         >
-          <div className="absolute bg-lightRed w-64 h-64 rounded-full top-8 left-9"></div>
+          <div className="absolute bg-lightRed w-[17rem] h-[17rem] rounded-full top-[5rem] left-8"></div>
           <Image
             className="relative"
-            src={"/KruProfile.png"}
+            src={"/kru-bg.png"}
             alt="profile"
-            width={320}
-            height={320}
+            width={340}
+            height={340}
           />
         </motion.div>
       </div>
